b5bb7841e551008c3b9912de42bacc78862d202f
Bartosz Nowak <bnowak@int.pl>
2021-05-30 16:17:52 +0200

    Initialize dummy Slick carousel

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import 'bootstrap';
+import 'slick-carousel';
 import $ from "jquery";
 
 
@@ -9,5 +10,18 @@ $(document).ready(() => {
     $(() => {
         $('[data-toggle="tooltip"]').tooltip({ trigger: "hover" })
     }); 
+
+    $('.carousel-secondary').slick({
+        autoplay: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        dots: false,
+        arrows: true,
+        centerMode: true,
+        //centerPadding: '30px',
+        focusOnSelect: true,
+        //prevArrow: '<a class="slick-prev" href="#"><i data-icon="ei-arrow-left" data-size="m"></i></a>',
+        //nextArrow: '<a class="slick-next" href="#"><i data-icon="ei-arrow-right" data-size="m"></i></a>',
+    });
 
 });
